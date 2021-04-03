08363c7bf5a5c4c499546e9bd58b891215995999
Paul Burke <paul@paulburke.co>
2021-04-03 11:20:54 -0400

    Adds follower count and follows you label to profile pages

--- main.js
+++ main.js
@@ -19,25 +19,99 @@ const getSpotPrice = function () {
   return 0
 }
 
-const getUserName = function () {
+const getLoggedInUserName = function () {
   if (username && username !== '') return username
-  
+
   const elementList = document.getElementsByClassName('change-account-selector__ellipsis-restriction')
 
   try {
     const changeAccountSelector = elementList.item(0)
-    username = changeAccountSelector.innerHTML.trim() 
+    username = changeAccountSelector.innerHTML.trim()
     return username
   } catch (e) {}
 
   return ''
 }
 
-const addNativeCoinPrice = function () {
+const getProfile = function (username) {
+  return fetch('https://api.bitclout.com/get-profiles', {
+    'headers': {
+      'accept': 'application/json, text/plain, */*',
+      'accept-language': 'en-US,en;q=0.9',
+      'content-type': 'application/json',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+      'sec-gpc': '1'
+    },
+    'referrerPolicy': 'no-referrer',
+    'body': JSON.stringify({
+      PublicKeyBase58Check: '',
+      Username: username,
+      UsernamePrefix: '',
+      Description: '',
+      OrderBy: 'newest_last_post',
+      NumToFetch: 1,
+      ModerationType: '',
+      FetchUsersThatHODL: true,
+      AddGlobalFeedBool: false
+    }),
+    'method': 'POST',
+    'mode': 'cors',
+    'credentials': 'include'
+  }).then(res => res.json())
+    .then(res => res.ProfilesFound[0])
+}
+
+const getUser = function (key) {
+  return fetch('https://api.bitclout.com/get-users-stateless', {
+    'headers': {
+      'accept': 'application/json, text/plain, */*',
+      'accept-language': 'en-US,en;q=0.9',
+      'content-type': 'application/json',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+      'sec-gpc': '1'
+    },
+    'referrerPolicy': 'no-referrer',
+    'body': JSON.stringify({
+      PublicKeyBase58Check: key
+    }),
+    'method': 'POST',
+    'mode': 'cors',
+    'credentials': 'include'
+  }).then(res => res.json())
+    .then(res => res.userData.userList[0])
+}
+
+const getFollowing = function (username) {
+  return fetch('https://api.bitclout.com/get-follows-stateless', {
+    'headers': {
+      'accept': 'application/json, text/plain, */*',
+      'accept-language': 'en-US,en;q=0.9',
+      'content-type': 'application/json',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+      'sec-gpc': '1'
+    },
+    'referrerPolicy': 'no-referrer',
+    'body': JSON.stringify({
+      PublicKeyBase58Check: '',
+      username: username,
+      getEntriesFollowingPublicKey: false,
+    }),
+    'method': 'POST',
+    'mode': 'cors',
+    'credentials': 'include'
+  }).then(res => res.json())
+}
+
+const addNativeCoinPrice = function (topCard) {
   const nativePriceId = 'plus-native-price'
   if (document.getElementById(nativePriceId)) return
 
-  const topCard = document.querySelector('creator-profile-top-card')
   try {
     const userDataDiv = topCard.firstElementChild.children.item(3)
     const coinPriceDiv = userDataDiv.lastElementChild.lastElementChild
@@ -45,13 +119,13 @@ const addNativeCoinPrice = function () {
     const price = parseFloat(coinInDollars.slice(2, coinInDollars.length))
 
     const spotPrice = getSpotPrice()
-    const nativePrice = (price / spotPrice).toFixed(3)
+    const nativePrice = (price / spotPrice).toFixed(1)
 
     let span = document.createElement('span')
     span.id = nativePriceId
     span.className = 'fc-muted mr-2 fs-13px'
     span.style.fontWeight = '500'
-    span.innerText = `(${nativePrice} BTCLT)`
+    span.innerText = `(${nativePrice} BC)`
     coinPriceDiv.insertBefore(span, coinPriceDiv.lastElementChild)
   } catch (e) {}
 }
@@ -100,7 +174,7 @@ const addHoldersCount = function (profileDetails) {
   } catch (e) {}
 }
 
-const addHolderPercentages = function (profileDetails) {
+const addHolderPercentages = function (profileDetails, topCard) {
   if (!profileDetails) return
 
   const holderPercentageClassName = 'plus-profile-holder-share'
@@ -118,7 +192,6 @@ const addHolderPercentages = function (profileDetails) {
       const coinsHeld = parseFloat(heldColumnItem.innerHTML)
 
       if (!isNaN(coinsHeld)) {
-        const topCard = document.querySelector('creator-profile-top-card')
         const valueBar = topCard.firstElementChild.lastElementChild
         const circulationContainer = valueBar.firstElementChild
         const circulationHtml = circulationContainer.firstElementChild.innerHTML.trim()
@@ -144,7 +217,7 @@ const addHolderRankings = function (profileDetails) {
     const holderContainer = holderDiv.children.item(1)
     const holderList = holderContainer.firstElementChild
 
-    const username = getUserName()
+    const username = getLoggedInUserName()
     let startingIndex = 2
 
     const firstHolderItem = holderList.children.item(1)
@@ -182,7 +255,7 @@ const highlightUserInHolderList = function (profileDetails) {
     const holderContainer = holderDiv.children.item(1)
     const holderList = holderContainer.firstElementChild
 
-    const username = getUserName()
+    const username = getLoggedInUserName()
 
     // Skip the first two and last item
     for (let i = 2; i < (holderList.childElementCount - 1); i++) {
@@ -203,17 +276,80 @@ const highlightUserInHolderList = function (profileDetails) {
   } catch (e) {}
 }
 
+const addFollowingEnrichments = function (topCard) {
+  if (!topCard) return
+
+  const followingCountId = 'plus-profile-following-count'
+  if (document.getElementById(followingCountId)) return
+
+  try {
+    getProfile(getLoggedInUserName())
+      .then(loggedInProfile => {
+        if (document.getElementById(followingCountId)) return
+
+        const loggedInKey = loggedInProfile.PublicKeyBase58Check
+
+        const segments = new URL(document.location).pathname.split('/');
+        const profileUsername = segments.pop() || segments.pop();
+
+        getProfile(profileUsername)
+          .then(profile => {
+            if (document.getElementById(followingCountId)) return
+
+            const key = profile.PublicKeyBase58Check
+
+            return getFollowing(profileUsername).then(followingRes => {
+              if (document.getElementById(followingCountId)) return
+
+              const userDataDiv = topCard.firstElementChild.children.item(3)
+
+              const following = followingRes.PublicKeyToProfileEntry
+              let followsYou = following[`${loggedInKey}`] !== undefined
+              if (followsYou) {
+                const usernameDiv = userDataDiv.firstElementChild
+
+                const followsYouSpan = document.createElement('span')
+                followsYouSpan.className = 'plus-profile-follows-you ml-3 fs-12px font-weight-normal text-grey5 br-12px'
+                followsYouSpan.innerText = 'Follows you'
+
+                usernameDiv.appendChild(followsYouSpan)
+              }
+
+              const bottomDiv = userDataDiv.lastElementChild
+
+              const countSpan = document.createElement('span')
+              countSpan.className = 'font-weight-bold'
+              countSpan.innerText = `${followingRes.NumFollowers}`
+
+              const labelSpan = document.createElement('span')
+              labelSpan.className = 'fc-muted'
+              labelSpan.innerHTML = 'Following&nbsp;&nbsp;'
+
+              const a = document.createElement('a')
+              a.id = followingCountId
+              a.className = 'link--unstyled'
+              a.href = document.location.pathname + '/following'
+              a.innerHTML = `${countSpan.outerHTML} ${labelSpan.outerHTML}`
+
+              bottomDiv.insertBefore(a, bottomDiv.lastElementChild)
+            })
+          })
+
+      })
+  } catch (e) {}
+}
+
 const addEditProfileButton = function () {
   let editProfileButtonId = 'plus-sidebar-edit-profile'
   if (document.getElementById(editProfileButtonId)) return
 
   const leftBarButtons = document.querySelectorAll('left-bar-button')
   try {
-    Array.from(leftBarButtons).forEach(button => {
+    leftBarButtons.forEach(button => {
       const profileDiv = button.firstElementChild.lastElementChild
       const profileAnchor = profileDiv.firstElementChild
 
-      if (profileAnchor.innerHTML === "Profile") {
+      if (profileAnchor.innerHTML === 'Profile') {
         const a = document.createElement('a')
         a.id = editProfileButtonId
         a.href = 'update-profile'
@@ -237,9 +373,9 @@ const addNewPostButton = function () {
     let button = document.createElement('button')
     button.id = addPostButtonId
     button.type = 'button'
-    button.className = 'ml-5 btn btn-primary font-weight-bold fs-14px pl-5 pr-5'
+    button.className = 'btn btn-secondary font-weight-bold fs-14px'
     button.innerText = 'Post'
-    button.onclick = ev => window.location.href='posts/new'
+    button.onclick = ev => window.location.href = 'posts/new'
 
     globalNav.appendChild(button)
   } catch (e) {}
@@ -249,10 +385,12 @@ const toggleSidebar = function () {
   // New post screen is typically only used for mobile, so we need to hide the sidebar
   const createPostPage = document.querySelector('app-create-post-page')
   const leftBar = document.querySelector('left-bar')
-  if (createPostPage) {
-    leftBar.style.display = 'none'
-  } else {
-    leftBar.style.display = 'inherit'
+  if (leftBar) {
+    if (createPostPage) {
+      leftBar.style.display = 'none'
+    } else {
+      leftBar.style.display = 'inherit'
+    }
   }
 }
 
@@ -262,11 +400,15 @@ const enrichProfile = function () {
 
   addSellButton()
 
-  addNativeCoinPrice()
+  const topCard = document.querySelector('creator-profile-top-card')
+
+  addNativeCoinPrice(topCard)
+
+  addFollowingEnrichments(topCard)
 
   addHoldersCount(profileDetails)
 
-  addHolderPercentages(profileDetails)
+  addHolderPercentages(profileDetails, topCard)
 
   highlightUserInHolderList(profileDetails)
 
