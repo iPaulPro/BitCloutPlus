9d2c61b0a59af58b5405b06d70341dd537ddb81b
Paul Burke <paul@paulburke.co>
2022-03-07 21:00:57 -0500

    Switches Omnibox keyword to "deso"; Allows for querying by public key

--- background.js
+++ background.js
@@ -4,8 +4,44 @@
  Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
  */
 
+const PUBLIC_KEY_PREFIX = 'BC1YL'
+const PUBLIC_KEY_LENGTH = 55
+
+const getUsernameForPublicKey = (publicKey) => {
+  if (!publicKey) return Promise.reject('Missing required parameter publicKey')
+
+  return fetch(`https://node.deso.org/api/v0/get-user-name-for-public-key/${publicKey}`)
+    .then(res => res.json())
+    .then(atob)
+}
+
+const isPublicKeyBase58Check = (query) => {
+  return query.startsWith(PUBLIC_KEY_PREFIX) && query.length === PUBLIC_KEY_LENGTH
+}
+
+const openUserInTab = async (username) => {
+  const newURL = 'https://node.deso.org/u/' + username
+  await chrome.tabs.create({url: newURL})
+}
+
 chrome.omnibox.onInputEntered.addListener(async text => {
   // Encode user input for special characters , / ? : @ & = + $ #
-  const newURL = 'https://bitclout.com/u/' + encodeURIComponent(text)
-  await chrome.tabs.create({url: newURL})
+  const query = encodeURIComponent(text)
+
+  if (isPublicKeyBase58Check(query)) {
+    let username
+
+    try {
+      username = await getUsernameForPublicKey(query)
+    } catch (e) {
+      console.error(`No username found for '${query}'`, e)
+    }
+
+    if (username) {
+      await openUserInTab(username)
+      return
+    }
+  }
+
+  await openUserInTab(query)
 })
